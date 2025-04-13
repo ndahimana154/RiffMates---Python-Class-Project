d367a995bc218917aede1ca272dfb8bad84e3542
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-04-13 23:48:17 +0800

    First commt -m

---
+++ home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# View for credits (text/plain response)
+def credits(request):
+    content = "Nick\nNdahimana"
+    return HttpResponse(content, content_type="text/plain")
+
+# View for about (JSON API response)
+def about(request):
+    return JsonResponse({
+        "version": "1.0.0",
+        "message": "This is the about page (API response)."
+    })
+
+# View for news (HTML page with dynamic content)
+def news(request):
+    data = {
+        "news": [
+            "This is the latest news content.",
+            "This is another news content."
+        ]
+    }
+    return render(request, 'news.html', data)
